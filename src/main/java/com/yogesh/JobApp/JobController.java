e1ba9757ce818b397fc5665934f0d7de9656273b
yogesh314 <yogeshatworld3.14@gmail.com>
2025-02-28 01:14:39 +0530

    7. Adding View all jobs functionality.

--- src/main/java/com/yogesh/JobApp/JobController.java
+++ src/main/java/com/yogesh/JobApp/JobController.java
@@ -4,8 +4,11 @@ import com.yogesh.JobApp.model.JobPost;
 import com.yogesh.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -27,6 +30,13 @@ public class JobController {
     public String handleForm(JobPost jobPost) {             //If we want to send the data we need to accept it first
         service.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model model){
+        List<JobPost> jobs = service.getAllJobs();
+        model.addAttribute("jobPosts",jobs);
+        return "viewalljobs";
     }
 }
 
